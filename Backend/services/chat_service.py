ee6d3b0f419941d98760531857cf3e03c107255e
Tnghia9001 <mnghia9933@gmail.com>
2025-08-02 16:42:30 +0700

    update for function call

--- Backend/services/chat_service.py
+++ Backend/services/chat_service.py
@@ -1,6 +1,12 @@
 import openai
 import os
 import json
+import re
+
+from services.ticket_service import TicketService
+from routers.ticket_router import create_ticket
+from routers.ticket_router import update_ticket
+from routers.ticket_router import delete_ticket
 
 class ChatService:
     def __init__(self, chat_data_path: str = "storage/data/messages.json"):
@@ -21,8 +27,24 @@ class ChatService:
             base_url="https://aiportalapi.stu-platform.live/use",
             api_key="sk-DRKoljlUoP4FtPCOBVy71Q"
         )
+
+        functions_ = TicketService.get_all_functions()
         response = client.chat.completions.create(
             model="Gemini-2.5-Flash",
-            messages=self.prepare_messages(messages)
+            messages=self.prepare_messages(messages),
+            functions=functions_,
+            function_call="auto",
         )
+
+        function_call = response.choices[0].message.function_call
+        if function_call:
+            function_name = function_call.name
+            function_args = json.loads(function_call.arguments)
+            if function_name == "create_ticket":
+                create_ticket(function_args)
+            elif function_name == "update_ticket":
+                update_ticket(function_args)
+            elif function_name == "delete_ticket":
+                delete_ticket(function_args)
+
         return response.choices[0].message.content
