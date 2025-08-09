0a9637c82740a5d2c5b848068799bacaf268f3f7
Phúc <68469045+phucpt25@users.noreply.github.com>
2025-08-09 10:48:53 +0700

    feat(chromaDB): init config for chromaDB

--- Backend/routers/__init__.py
+++ Backend/routers/__init__.py
@@ -1,5 +1,6 @@
 from .ticket_router import router as ticket_router
 from .conversation_router import router as conversation_router
 from .chat_router import router as chat_router
+from .chroma_router import router as chroma_router
 
-__all__ = ['ticket_router', 'conversation_router', 'chat_router'] 
+__all__ = ['ticket_router', 'conversation_router', 'chat_router', 'chroma_router'] 
