ddb88e93e094a05ba14d29c9aa4141426a286fa4
Tuan Nguyen <>
2025-08-22 19:07:27 +0700

    Support env for FE

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
+// Environment variables prefixed with VITE_ will be exposed to your Vite-processed code
 export default defineConfig({
   plugins: [react()],
   resolve: {
